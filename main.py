790b887c224e6b61c5fdbd001cdd19801e4f17e0
Kintobor <rohan.s.malhi@gmail.com>
2021-12-10 12:23:03 +0530

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Fortnite')
+from pytube import YouTube
+
+filePath = input("What should the files path be?\n")
+link = input('What is the URL of your video?\n')
+
+try:
+    yt = YouTube(link)
+except:
+    print('Connection error')
+    exit()
+
+mp4files = yt.filer('mp4')
+
+yt.set_filename(yt.title)
+
+dVideo = yt.get(mp4files[-1].extension, mp4files[-1].resolution)
+
+try:
+    dVideo.download(filePath)
+except:
+    print('Error')
+
+print('Done')
